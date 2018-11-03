f60cdd42daab8ef97fcf1cca080ff0b26cd0129f
Matthew Zvirbulis <matthewzvirbulis@students.federation.edu.au>
2018-11-03 21:40:27 +1100

    gamelogic test

---
+++ src/gameTests/GameLogicTests.java
@@ -0,0 +1,27 @@
+package gameTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import GameObjects.GameLogic;
+
+class GameLogicTests {
+
+	GameLogic testLogic = new GameLogic();
+	
+	@Test
+	public void testCoinFlip() {
+
+		assertTrue(testLogic.compareInts(20, 10));
+		
+		}
+
+	@Test 
+	
+	public void testCoinFlip2() {
+		
+		assertFalse(testLogic.compareInts(10, 20));
+		
+	}
+}
